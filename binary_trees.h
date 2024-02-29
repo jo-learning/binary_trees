cf32baa129edef98c23e56d9b4ecfd9c29df79fe
yohannes <yohannesguesh01@gmail.com>
2024-02-29 14:12:27 -0800

    binary trees

--- binary_trees.h
+++ binary_trees.h
@@ -1,3 +1,7 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -15,9 +19,6 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
 
@@ -37,3 +38,5 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+#endif /* _BINARY_TREES_H_ */
